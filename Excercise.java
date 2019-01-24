e1e2ce593508e6212affba709827d04d245813d8
Joan <jaoanan@zagmail.gonzaga.edu>
2019-01-24 13:06:44 -0800

    "File we went over in class"

---
+++ Excercise.java
@@ -0,0 +1,13 @@
+import javax.Swing.JOptionPane;
+
+public class Excrcise {
+  public static void main(String [] args)
+  {
+    String number;
+    int intNum;
+
+    JOptionPane.showInputDialog(null, "Greetings Earthling");
+    number = JOptionPane.showInputDialog( "Enter a number");
+    intNum = integer.parseInt(number);
+  }
+}
