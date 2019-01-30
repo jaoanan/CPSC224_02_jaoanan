1bf362dc874ac282b027dbf7c3eba4d22dbf1cc4
Joan <jaoanan@zagmail.gonzaga.edu>
2019-01-29 18:53:42 -0800

    Recent Jan 29

--- yahtzee1.java
+++ yahtzee1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class yahtzee1 {
   public static void main(String [] args) {
-
+    Scanner scan = new Scanner(System.in);
     final int DICE_IN_PLAY = 5;
     int[] hand = new int[DICE_IN_PLAY];
     char playAgain ='y';
@@ -12,87 +12,88 @@ public class yahtzee1 {
       String testString= {"nnnnn"}; // ask nadra about string and keep being an char
       char[] keep = testString.toCharArray();
       int turn = 1;
-    while(turn < 4 && keep != "yyyyy")
-    {
-      for(int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
+      while(turn < 4 && keep != "yyyyy")
       {
-        if(keep[dieNumber] !='y')
-        hand[dieNumber] = 5; // Stopped on line 32 of the .cpp file
+        for(int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
+        {
+          if(keep[dieNumber] !='y')
+            hand[dieNumber] = 5; // Stopped on line 32 of the .cpp file
+        }
+        System.out.println("Your roll was: ");
+        for (int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
+        {
+          System.out.println(hand[dieNumber] + " ");
+        }
+        System.out.println();
+        if (turn < 3)
+        {
+          System.out.println("Enter dice to keep (y or n)");
+          keep = scan.nextLine();
+        }
+        turn++;
       }
-      System.out.println("Your roll was: ");
-
-      for (int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
+      sortArray(hand, DICE_IN_PLAY);
+      System.out.println("Here is your sorted hand : ");
+      for(int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
       {
         System.out.println(hand[dieNumber] + " ");
       }
       System.out.println();
-      if (turn < 3)
+      for(int dieValue = 1; dieValue <=6; dieValue++)
       {
-        System.out.println("Enter dice to keep (y or n)");
-        keep = sc.nextLine();
+        int currentCount = 0;
+      for(int diePosition = 0; diePosition < 5; diePosition++)
+      {
+        if (hand[diePosition] == dieValue)
+          currentCount++;
       }
-      turn++;
-    }
-    sortArray(hand, DICE_IN_PLAY);
-    System.out.println("Here is your sorted hand : ");
-    for(int dieNumber = 0; dieNumber < DICE_IN_PLAY; dieNumber++)
-    {
-      System.out.println(hand[dieNumber] + " ");
-    }
-    System.out.println();
-    for(int dieValue = 1; dieValue <=6; dieValue++)
-    {
-      int currentCount = 0;
-    for(int diePosition = 0; diePosition < 5; diePosition++)
-    {
-      if (hand[diePosition == dieValue])
-      currentCount++;
-    }
-    System.out.println( "Score " + dieValue * currentCount + " on the "
-      + dieValue + " line");
-    }
-    if (maxOfAKindFound(hand) >= 3) {
-      System.out.println("Score " + totalAllDice(hand) << " on the ");
-      System.out.println("3 of a Kind line");
-    }
-    else
-    {
-      System.out.println("Score 0 on the 3 Kind line");
-    }
-    if (maxOfAKindFound(hand) >= 4)
-    {
-      System.out.println("Score" + totalAllDice(hand) +" on the ");
-      System.out.println("4 of Kind line");
-    }
-    else
-      {
-      System.out.println("Score 0 on the 4 of a Kind line");
+      System.out.println( "Score " + dieValue * currentCount + " on the "
+        + dieValue + " line");
       }
-    if (fullHouseFound(hand))
+      if (maxOfAKindFound(hand) >= 3)
       {
-      System.out.println("Score 25 on the Full House line")
+        System.out.println("Score " + totalAllDice(hand) << " on the ");
+        System.out.println("3 of a Kind line");
+      }
+      else
+      {
+        System.out.println("Score 0 on the 3 Kind line");
+      }
+      if (maxOfAKindFound(hand) >= 4)
+      {
+        System.out.println("Score" + totalAllDice(hand) +" on the ");
+        System.out.println("4 of Kind line");
+      }
+      else
+      {
+        System.out.println("Score 0 on the 4 of a Kind line");
+      }
+      if (fullHouseFound(hand))
+      {
+        System.out.println("Score 25 on the Full House line");
       }
     if (maxStraightFound(hand) >= 4)
-    System.out.println("Score 30 on the Small Straight line");
+      System.out.println("Score 30 on the Small Straight line");
     else {
       System.out.println("Score 0 on the Small Straight line");
-    }
+      }
     if (maxStraightFound(hand) >= 5)
-    System.out.println("Score 40 on the Large Straight line");
-    else {
-      System.out.println("Score 0 on the Large Straight line");
+      System.out.println("Score 40 on the Large Straight line");
+    else
+        System.out.println("Score 0 on the Large Straight line");
     if (maxOfAKindFound(hand) >= 5)
       System.out.println("Score 50 on the Yahtzee line");
     else {
       System.out.println("Score 0 on the Yahtzee line");
       System.out.println("Score " + totalAllDice(hand) +" on the ");
       System.out.println("Chance line");
-      Scanner scan = new Scanner(System.in)
+      Scanner scan = new Scanner(System.in);
       System.out.print("\n Enter 'y' to play again "); //is this corre
-      playAgain= keyboard.nextInt();
+      playAgain= scan.nextInt();
     }
   return 0;
   }
+}
 
   public static int rollDie()
 //this function simulates the rolling of a single die
@@ -132,7 +133,7 @@ return maxCount;
   public static void sortArray(int array[], int size)
 //bubble sort from Gaddis chapter 8
   {
-    bool swap;
+    boolean swap;
     int temp;
     do
     {
@@ -166,12 +167,12 @@ return maxCount;
     }
   return maxLength;
   }
-  public static bool fullHouseFound(int hand[])
+  public static boolean fullHouseFound(int hand[])
   {
-    bool foundFH = false;
-    bool found3K = false;
-    bool found2K = false;
-    int currentCount ;
+    boolean foundFH = false;
+    boolean found3K = false;
+    boolean found2K = false;
+    int currentCount;
     for (int dieValue = 1; dieValue <=6; dieValue++)
     {
         currentCount = 0;
