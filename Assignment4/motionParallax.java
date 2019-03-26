53649cf9a605e6752eff30f8db73b8b3bf6d46c3
aburka01 <43026339+aburka01@users.noreply.github.com>
2019-03-25 22:51:42 -0700

    Update motionParallax.java

--- Assignment4/motionParallax.java
+++ Assignment4/motionParallax.java
@@ -5,19 +5,20 @@ import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-public class motionParallax extends JFrame
+public class motionParallax extends JFrame //Calls motionParallax so that the functions are able to move within the frame
 {
+//references the TimerPanel class in TimerPanel.java
   private JPanel TimerPanel;
 
   public motionParallax()
   {
     JFrame frame = new JFrame( "Motion Parallax" );
-    frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+    frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE ); 
     TimerPanel tp = new TimerPanel();
     frame.add(tp);
     frame.add(new TimerPanel());
-    frame.setSize( 500, 500 ); // set frame size
-    frame.setVisible( true );
+    frame.setSize( 500, 500 ); // set the frame size 
+    frame.setVisible( true ); 
     frame.setResizable (false);
   }
 
